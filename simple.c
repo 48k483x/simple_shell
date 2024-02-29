7cb3d43291fce607159617957d30a7aef960d62e
ABDURRAHMAN <123483885+48k483x@users.noreply.github.com>
2024-02-29 02:34:10 +0000

    🚀 Just launched my simple_shell project! 💻 Starting this journey into coding like 🌱. Excited to see where it takes me! 😄

--- simple.c
+++ simple.c
@@ -1,22 +1,12 @@
 #include "simple.h"
 
-/*
- * This is a simple shell program that implements basic shell functionalities.
- * It allows users to execute commands, change directories, and handle process management.
- * The program uses various system calls and library functions to achieve these functionalities.
- * Allowed functions include:
- *			access() chdir() close() closedir() execve() exit() _exit() fflush() fork() free() getcwd() getline() getpid() isatty()
- * 			kill() malloc() open() opendir() perror() read() readdir() signal() stat() lstat() fstatt() strtok() wait() waitpid() 
- * 			wait3() wait4()  write()
- */
-
-char *exit_message(int err_no, char *line)
+char
+exit_message(int err_no, char *line)
 {
 	perror("Error");
 	free(line);
 	exit(err_no);
 }
-
 int
 main(int ac, char **av)
 {
@@ -50,6 +40,7 @@ main(int ac, char **av)
 					char *program = strtok(line, " ");
 					char *arg = strtok(NULL, " ");
 					char *args[] = {program, arg, NULL};
+
 					if (execvp(args[0], args) == -1)
 						exit_message(2, line);
 				}
